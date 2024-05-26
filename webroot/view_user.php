b0401ea3b368005dac838c5232097889f71d2ef7
6410685157 <6410685157@student.tu.ac.th>
2024-05-27 00:40:15 +0700

    add view user

--- webroot/view_user.php
+++ webroot/view_user.php
@@ -4,16 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 </head>
 <body>
-    <form action="view_user.php" method="post">
-        <label for="id">ID : </label>
-        <input type="text" name="id" id="id" required><br>
+    <form action="view_user.php" method="post" class="container mt-3">
 
+        <h1 class="display-4">Welcome</h1>
+
+        <div class="mb-3">
+            <label for="id">ID : </label>
+            <input type="text" name="id" id="id" required>
+        </div>
         <input type="hidden" name="idCheck" value="1">
-
-        <input type="submit">
-        <input type="reset">
+        <input type="submit" class="btn btn-primary">
+        <input type="reset" class="btn btn-warning">
     </form>
     <?php 
         if ( isset($_POST['idCheck'])){
